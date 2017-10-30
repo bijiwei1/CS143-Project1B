1cd3bfb2b0ae0e1fdec3b55f4309b8dffef67c87
bijiwei1 <bibijijiweihch@gmail.com>
2017-10-30 02:07:40 -0700

    fixed

--- www/showMovie.php
+++ www/showMovie.php
@@ -2,9 +2,19 @@
 <head>
     <style>
     table, td, th {
-        border: 1px solid black;
+        border: 1px grey;
     }
     </style>
 </head>
 
+<body>
+<div class="nav_bar">
+	 <form action="./search.php" method="GET">
+        Search:
+        <input type="text" name="Search..." />
+        <input type="submit" value="Click Me!" />
+    </form>
+</div>	
+
+</body>
 </html>
